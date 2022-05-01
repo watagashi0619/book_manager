35483414355b04d0c3447777a36f85e8559a325e
potagashi <watagashi0619@gmail.com>
2022-05-02 02:52:52 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <BarcodeScanner />
+  </div>
+</template>
+
+<script>
+import BarcodeScanner from "./components/BarcodeScanner";
+
+export default {
+  name: "App",
+  components: {
+    BarcodeScanner,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  devServer: {
+    https: true,
+  },
+  transpileDependencies: true,
+  configureWebpack: {
+    watch: true
+  }
+})
